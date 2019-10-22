c6e27afc2590ed683036964869c3f0854b217f9e
Allan Burns <allanlburns@yahoo.com>
2019-10-22 14:30:56 -0400

    Completed form pages. TODO: add posting, profile, database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from config import Config
 
 app = Flask(__name__)
+
+# all variables based on this application must be instantiated after the app instance
+
+
 bootstrap = Bootstrap(app)
+
+# flask app instance uses from object method to load in all configuration variables
+
+# this loads all the config attributes into app
+app.config.from_object(Config)
 
 from app import routes
