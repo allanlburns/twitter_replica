304efafbfdcc8019d1d130711eb5f2806077b278
Allan Burns <allanlburns@yahoo.com>
2019-10-30 14:34:36 -0400

    Commit Profile for deployment.

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,9 @@
 from app import app, db
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, jsonify, request
 from app.forms import TitleForm, ContactForm, LoginForm, RegisterForm, PostForm
 from app.models import Post, User, Contact
 from flask_login import login_user, logout_user, login_required, current_user
+import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -160,3 +161,147 @@ def profile(username=''):
 def logout():
     logout_user()
     return redirect(url_for('login'))
+
+
+################################################################################
+################################################################################# API section
+################################################################################################################################################################
+
+
+# Create an API that handles getting tweets
+@app.route('/api/posts/retrieve/', methods=['GET'])
+def api_posts_retrieve():
+# try:
+    # get variables passed into api
+    username = request.args.get('username')
+    month = request.args.get('month')
+
+    if username and month:
+        user = User.query.filter_by(username=username).first()
+        posts = user.posts
+
+        data = []
+
+        for post in posts:
+
+            if post.date_posted.month == int(month):
+                data.append({
+                    'post_id': post.post_id,
+                    'user_id': post.user_id,
+                    'tweet': post.tweet,
+                    'date_posted': post.date_posted
+                })
+
+        return jsonify({ 'code' : 200, 'data' : data})
+
+
+    if username:
+        user = User.query.filter_by(username=username).first()
+        posts = user.posts
+
+        data = []
+
+        # d = datetime.datetime(2019, 1, 1, 1, 00)
+        # post = Post(user_id=1, tweet='Happy New Year!', date_posted=d)
+        # db.session.add(post)
+        # db.session.commit()
+
+        # d = datetime.datetime(2019, 10, 24, 11, 00)
+        # post = Post(user_id=1, tweet='Hello from today!', date_posted=d)
+        # db.session.add(post)
+        # db.session.commit()
+
+        for post in posts:
+            data.append({
+                'post_id': post.post_id,
+                'user_id': post.user_id,
+                'tweet': post.tweet,
+                'date_posted': post.date_posted
+            })
+
+        return jsonify({ 'code' : 200, 'data' : data})
+    return jsonify({ 'code' : 200, 'message': 'Invalid params' })
+
+# except:
+    return jsonify({
+        'message': 'Error #1001: Something went wrong',
+        'code': 1001
+    })
+
+# create an API that handles posting tweets
+@app.route('/api/posts/save/', methods=['POST'])
+def api_posts_save():
+    '''
+        This API route should take in two arguments,
+        username, tweet, and save the twee into the database,
+        then return a success or fail message. Be sure to set date_posted
+        to current time.
+    '''
+    # user_id
+    username = request.args.get('username')
+    # tweet
+    tweet = request.args.get('tweet')
+
+    user = User.query.filter_by(username=username).first()
+
+    if user:
+
+        post = Post(username=username, tweet=tweet)
+        db.session.add(post)
+        db.session.commit()
+
+        return jsonify({ 'code' : 200, 'message': 'Tweet saved.' })
+
+    # try:
+    #     # get variables passed into api
+    #     username = request.args.get('username')
+    #     print(username)
+    #
+    #     return jsonify({ 'code' : 200 })
+    #
+    # except:
+    #     return jsonify({
+    #         'message': 'Error #1002: Something went wrong',
+    #         'code': 1002
+    #     })
+
+# api to delete posts, take in a post id and delete
+@app.route('/api/posts/delete/', methods=['DELETE'])
+def api_posts_delete():
+    try:
+        post_id = request.args.get('post_id')
+
+        post = Post.query.get(int(post_id))
+
+        db.session.delete(post)
+        db.session.commit()
+        return jsonify({ 'code': 200, 'message': 'Tweet deleted'})
+    except:
+        return jsonify({ 'code': 1004, 'message': 'Couldn\'t find post to delete' })
+# this api should accept info through headers securely, and pass back infor of user
+
+@app.route('/api/users/retrieve/', methods=['GET', 'POST'])
+def api_users_retrieve():
+    try:
+        # get headers first
+        username = request.headers.get('username')
+        API_KEY = request.headers.get('API_KEY')
+
+        if API_KEY == 'secret':
+            # query the user based on username, get info, return it
+            user = User.query.filter_by(username=username).first()
+
+            info = {
+                'first_name': user.first_name,
+                'last_name': user.last_name,
+                'age': user.age,
+                'username': user.username,
+                'email': user.email
+            }
+            return jsonify({ 'code': 200, 'data':info })
+        return jsonify({ 'code': 200, 'message': 'Invalid key'})
+    except:
+        return jsonify({
+            'message': 'Error #1003: Please dont\' be mad at me.',
+            'code': 1003
+        })
